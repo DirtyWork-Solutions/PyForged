0a1f36fde4986efd0a152055c6fbf3f50c44c73b
aidan <aidankhogg@gmail.com>
2025-03-05 06:25:41 +0000

    updated requirements
    - pydantic
    - pytest & pytest-metadata
    
    were both added

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='0.2.0',
     packages=find_packages(),
     install_requires=[
-        'antlr4-python3-runtime, 4.9.3',
+        'antlr4-python3-runtime~=4.9.3',
+        'pydantic~=2.10.6'
+        'pytest~=8.3.5',
+        'pytest-metadata~=3.1.1',
         ],
     entry_points={
         'console_scripts': [],
@@ -21,8 +24,20 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console'
+        'Framework :: Pytest',
+        'Framework :: Pydantic'
+        'Intended Audience :: Developers',
+        'Topic :: System',
+        'Topic :: System :: Logging',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Topic :: System :: Installation/Setup'
+
     ],
     python_requires='>=3.13',
 )
