e1afce1575961e874163d639eadfb84516b55ffe
aidan <aidankhogg@gmail.com>
2025-03-21 09:54:36 +0000

    temp dev publishing

--- pyforged/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = "DirtyWork Solutions Limited"
-__version__ = '0.2.7dev-a'
-__licence__ = 'MIT'
--- pyforged/__models__.py
+++
@@ -1,6 +0,0 @@
-
-#
-try:
-    from pydantic import BaseModel
-except ModuleNotFoundError as e:  # TODO: Log missing pydantic
-    pass
